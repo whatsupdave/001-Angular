e0f71df12f0d088c3e8c6b731fff6cf484c19b5a
Eivydas Kantautas <kantautaseivydas@gmail.com>
2022-04-07 22:03:45 +0300

    updated

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
-
-  public ugis:number = 100;
-  public svoris:number = 90;
-  public kmi:number = 0;
+  
+  public ugis?:number;
+  public svoris?:number;
+  public kmi:number=0;
+  public info:string='';
 
   constructor() { }
 
@@ -17,7 +18,15 @@ export class BodyComponent implements OnInit {
   }
 
   public skaiciuoti(){
-    this.kmi = this.svoris / (this.ugis/100)**2;
+    if (this.ugis!=null && this.svoris!=null){
+      this.kmi= Math.round(this.svoris / (this.ugis/100)**2) ;
+      if (this.kmi<25) 
+        this.info="Sveikiname Jūsų svoris normalus.";
+      if (this.kmi>=25 && this.kmi<30) 
+        this.info="Jūs turėtumėte maitintis sveikiau, KMI šiektiek didesnis";
+      if (this.kmi>=30)
+         this.info="Jūs turėtumėte sunerimti, KMI perdidelis";
+    }
   }
 
 }
